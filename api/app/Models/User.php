7d51927cca2c41f15429fef56006f8e354cc03b8
Shuichi Minamie <s.minamie@gmail.com>
2021-01-17 17:33:17 +0000

    add relational setting

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+       public function work()
+    {
+        return $this->belongsToMany(Work::class)->withTimestamps();
+    }
 }
