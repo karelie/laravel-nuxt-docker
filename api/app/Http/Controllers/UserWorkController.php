b97aceb835dbe0f88559e102a78cafb5b7b128b7
Shuichi Minamie <s.minamie@gmail.com>
2021-01-17 10:58:11 +0000

    Add API Work, UserWork

---
+++ api/app/Http/Controllers/UserWorkController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserWork;
+use Illuminate\Http\Request;
+
+class UserWorkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\UserWork  $userWork
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UserWork $userWork)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\UserWork  $userWork
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UserWork $userWork)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\UserWork  $userWork
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UserWork $userWork)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\UserWork  $userWork
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UserWork $userWork)
+    {
+        //
+    }
+}
---
+++ api/database/factories/UserWorkFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\UserWork;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserWorkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = UserWork::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
