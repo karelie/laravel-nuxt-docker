ef0cdbb37882c49ea3221f8a08467d5c1f56f0fe
Shuichi Minamie <s.minamie@gmail.com>
2021-01-18 09:58:42 +0000

    update user page

--- client/pages/user/_id.vue
+++ client/pages/user/_id.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    User
+    <h1>{{ user.name }}</h1>
     <nuxt-link to="/">Index</nuxt-link>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  head() {
+    return {
+      title: this.user.name,
+    };
+  },
+  async asyncData(context) {
+    const id = context.params.id;
+    const user = await context.$axios.$get("/api/user/" + id);
+    return { user };
+  },
+};
 </script>
