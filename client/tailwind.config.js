dcc0b74b900c77a6a16938c7ad5fb3ce8966138e
Shuichi Minamie <s.minamie@gmail.com>
2021-01-20 20:46:18 +0000

    update screen size

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-variants: {
+    variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
         display: ['group-hover'],
         borderWidth: ['last']
+        },
+    theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+
+            '2xl': '1536px',
+            // => @media (min-width: 1536px) { ... }
+            '3xl': '1726px',
+            // => @media (min-width: 1726px) { ... }
+        }
     }
 }
